0cea844c0fd2dcdf6a36301c713933a829a55791
Ankit Kumar <ankit.kumar4@appinventiv.com>
2025-02-12 16:38:50 +0530

    tailwindcss setup

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white dark:text-red-500 text-2xl">Hello, NativeWind!</Text>
+    </View>
+  );
+}
